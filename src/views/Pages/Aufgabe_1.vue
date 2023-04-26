7d6630c4234cd2bdecfb8465062cb01d4db6994c
Ben Streich <ben.streich@icloud.com>
2023-04-26 12:55:37 +0200

    vue commit 🙊🙊🙊🙊🙊🙊🙊🙊🙊🙊🙊🙊🙊🙊🙊

--- src/views/Pages/Aufgabe_1.vue
+++ src/views/Pages/Aufgabe_1.vue
@@ -1,5 +1,5 @@
 <template>
-  <Button :class="Background" :id="Text" >{{ Text }} auf {{ Background }}</Button>
+  <Button :class="Background" :id="Text" style="width: 200px; height: 50px; margin-top: 50px;">{{ Text }} auf {{ Background }}</Button>
 
   <h1>Hintergrund</h1>
   <p @click="Background = 'c1'">Grün</p>
@@ -8,11 +8,11 @@
   <p @click="Background = 'c4'">Rot</p>
   <p @click="Background = 'c5'">Schwarz</p>
   <p @click="Background = 'c6'">Weiss</p>
-
+ 
   <h1>Text</h1>
     <p @click="Text = 'c1'">Grün</p>
   <p @click="Text = 'c2'">Gelb</p>
-  <p @click="Text = 'c3'">Blau</p>
+  <p @click="Text = 'c3'">B lau</p>
   <p @click="Text = 'c4'">Rot</p>
   <p @click="Text = 'c5'">Schwarz</p>
   <p @click="Text = 'c6'">Weiss</p>
--- src/views/Pages/Aufgabe_2.vue
+++ src/views/Pages/Aufgabe_2.vue
@@ -1,9 +1,43 @@
-<template>
-  Aufgabe 2
-</template>
-
 <script setup>
+import { ref } from 'vue'
+
+let id = 0
+
+const newTodo = ref('')
+const hideCompleted = ref(false)
+const todos = ref([
+  { id: id++, text: 'augh', done: true },
+  { id: id++, text: 'hhhhhhhhhh', done: true },
+  { id: id++, text: 'sdkdjg', done: true }
+])
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value, done: false })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
 </script>
 
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+  
+</template>
+
 <style>
+.done {
+  text-decoration: line-through;
+}
 </style>
